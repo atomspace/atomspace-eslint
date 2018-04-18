c636cc88b77e611dd5937ef40b544f337da65bc1
constgen <const.gen@gmail.com>
2018-04-19 01:37:13 +0300

    Add JSDoc rules

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ let eslint = require('@neutrinojs/eslint');
 let coreConfig = require('./eslint.config.json');
 let importConfig = require('./import.config.json');
 let promiseConfig = require('./promise.config.json');
+let jsDocConfig = require('./jsdoc.config.json');
 
 let config = eslint.merge(
 	eslint.merge(
 		{ eslint: coreConfig },
 		{ eslint: importConfig }
 	),
-	{ eslint: promiseConfig }
+	eslint.merge(
+		{ eslint: promiseConfig },
+		{ eslint: jsDocConfig }
+	)
 );
 
 module.exports = function (neutrino, settings = {}) {
