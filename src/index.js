a5b985bfd3463d2a0fb6e951dfc876177524088f
constgen <const.gen@gmail.com>
2018-04-19 02:31:36 +0300

    Add AMD rules

--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ let coreConfig = require('./eslint.config.json');
 let importConfig = require('./import.config.json');
 let promiseConfig = require('./promise.config.json');
 let jsDocConfig = require('./jsdoc.config.json');
+let amdConfig = require('./amd.config.json');
 
-let config = eslint.merge(
-	eslint.merge(
-		{ eslint: coreConfig },
-		{ eslint: importConfig }
-	),
-	eslint.merge(
-		{ eslint: promiseConfig },
-		{ eslint: jsDocConfig }
-	)
-);
+let config = [
+	{ eslint: coreConfig },
+	{ eslint: importConfig },
+	{ eslint: promiseConfig },
+	{ eslint: jsDocConfig },
+	{ eslint: amdConfig }
+].reduce(eslint.merge);
 
 module.exports = function (neutrino, settings = {}) {
 	neutrino.use(eslint, eslint.merge(config, settings));
