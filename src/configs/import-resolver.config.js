15fbb5ea9d87e8de46c934dc51d189f75a194ddd
constgen <const.gen@gmail.com>
2020-08-29 22:02:54 +0300

    Fix Webpack modules resolving

--- src/configs/import-resolver.config.js
+++ src/configs/import-resolver.config.js
@@ -1,27 +1,15 @@
 module.exports = function importResolver (neutrinoConfig) {
 	let config = { settings: {} };
+	let resolve;
 
 	config.settings['import/resolver'] = {
 		webpack: {
 			config () {
-				let { resolve } = neutrinoConfig;
-
-				return {
-					resolve: {
-						extensions: resolve.extensions.values(),
-						alias: resolve.alias.entries(),
-						modules: resolve.modules.values(),
-						aliasFields: resolve.aliasFields.values(),
-						descriptionFiles: resolve.descriptionFiles.values(),
-						mainFields: resolve.mainFields.values(),
-						mainFiles: resolve.mainFiles.values(),
-						cachePredicate: resolve.get('cachePredicate'),
-						cacheWithContext: resolve.get('cacheWithContext'),
-						enforceExtension: resolve.get('enforceExtension'),
-						unsafeCache: resolve.get('unsafeCache'),
-						symlinks: resolve.get('symlinks')
-					}
-				};
+				// memoization for performance
+				if (!resolve) {
+					resolve = neutrinoConfig.resolve.toConfig();
+				}
+				return { resolve };
 			}
 		}
 	};
