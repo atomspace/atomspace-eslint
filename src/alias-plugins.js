ab3a069d41d2eac13db32352bcf68f915a57b283
constgen <const.gen@gmail.com>
2020-10-07 16:45:26 +0300

    Code refactoring

--- src/alias-plugins.js
+++ src/alias-plugins.js
@@ -1,24 +1,28 @@
 let moduleAlias = require('module-alias');
 
+function toFullName (pluginName) {
+	const ESLINT_PREFIX = 'eslint-plugin-';
+	const ORGANIZATION_EXPRESSION = /^(@[\d.A-z-]+)\/(.+)$/;
+	let nameIsFull = pluginName.indexOf(ESLINT_PREFIX) === 0;
+	let nameIsOrganization = ORGANIZATION_EXPRESSION.test(pluginName);
+
+	if (nameIsOrganization) {
+		let [, organizationName, name] = pluginName.match(ORGANIZATION_EXPRESSION);
+
+		return `${organizationName}/${toFullName(name)}`;
+	}
+
+	return nameIsFull ? pluginName : `${ESLINT_PREFIX}${pluginName}`;
+}
+
+function aliasToCurrentScope (pluginName) {
+	moduleAlias.addAlias(pluginName, require.resolve(pluginName));
+}
+
 module.exports = function aliasPlugins (config) {
 	let { plugins = [] } = config;
 
 	plugins
-		.map(function toFullName (pluginName) {
-			const ESLINT_PREFIX = 'eslint-plugin-';
-			const ORGANIZATION_EXPRESSION = /^(@[\d.A-z-]+)\/(.+)$/;
-			let nameIsFull = pluginName.indexOf(ESLINT_PREFIX) === 0;
-			let nameIsOrganization = ORGANIZATION_EXPRESSION.test(pluginName);
-
-			if (nameIsOrganization) {
-				let [, organizationName, name] = pluginName.match(ORGANIZATION_EXPRESSION);
-
-				return `${organizationName}/${toFullName(name)}`;
-			}
-
-			return nameIsFull ? pluginName : `${ESLINT_PREFIX}${pluginName}`;
-		})
-		.forEach(function (pluginName) {
-			moduleAlias.addAlias(pluginName, require.resolve(pluginName));
-		});
+		.map(toFullName)
+		.forEach(aliasToCurrentScope);
 };
