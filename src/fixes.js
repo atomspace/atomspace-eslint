fc7fe94dde2f1961a23f3c494d9b1746b1ee2840
constgen <const.gen@gmail.com>
2021-01-18 23:48:53 +0200

    Fix linting violations

--- src/fixes.js
+++ src/fixes.js
@@ -1,5 +1,4 @@
-
-let noSecretsPlugin = require("eslint-plugin-no-secrets");
+let noSecretsPlugin = require('eslint-plugin-no-secrets');
 
 // Remove JSON processors as they conflict with another JSON plugins
 delete noSecretsPlugin.processors;
